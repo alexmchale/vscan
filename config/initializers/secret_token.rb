193a3bc4e21e86dc8899ab5d5c63f0f85bd8775f
Alex McHale <alex@anticlever.com>
2014-03-26 07:30:22 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vscan::Application.config.secret_key_base = 'f4931e3561b8267b0d46b71dddc2e48236566ec1636a31c88027b8f46a098e6e4b48da11177f49bf0b984bc3fb028c643ee685f1d40832c1c22950b8a1d99149'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vscan::Application.config.session_store :cookie_store, key: '_vscan_session'
